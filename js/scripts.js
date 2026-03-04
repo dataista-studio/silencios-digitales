63b3376afccdf5a5365044309e056f83daa1a307
Svillalon <svillalon0@gmail.com>
2026-03-04 11:24:16 -0300

    Fix people div's in lg screens

--- js/scripts.js
+++ js/scripts.js
@@ -91,14 +91,14 @@ function peopleAnimation({
       const cell = document.createElement("div");
 
       if (whiteCells.has(`${r}-${c}`)) {
-        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[5.375rem] lg:h-[4.1875rem] ${whiteClass}`;
+        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[5.375rem] lg:h-[4rem] ${whiteClass}`;
         container.appendChild(cell);
         continue;
       }
 
       const type = cellMap.get(`${r}-${c}`);
 
-      cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[5.375rem] lg:h-[4.1875rem] ${
+      cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[5.375rem] lg:h-[4rem] ${
         type === "yellow" ? yellowClass : ""
       }`;
 
