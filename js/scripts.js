ff91351d9464029cd4ea027ef500fe19b0e6c383
Svillalon <svillalon0@gmail.com>
2026-03-05 18:06:49 -0300

    Fixing last styles

--- js/scripts.js
+++ js/scripts.js
@@ -91,7 +91,7 @@ function peopleAnimation({
       const cell = document.createElement("div");
 
       if (whiteCells.has(`${r}-${c}`)) {
-        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:h-[76.8px] lg:h-[60px] ${whiteClass}`;
+        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[3.87rem] lg:h-[3rem] ${whiteClass}`;
         container.appendChild(cell);
         continue;
       }
