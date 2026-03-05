146e06448d01f264a816b4ad35f696c22e20bad9
Svillalon <svillalon0@gmail.com>
2026-03-05 18:15:16 -0300

    Fixing last styles

--- js/scripts.js
+++ js/scripts.js
@@ -91,14 +91,14 @@ function peopleAnimation({
       const cell = document.createElement("div");
 
       if (whiteCells.has(`${r}-${c}`)) {
-        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[3.87rem] lg:h-[3rem] ${whiteClass}`;
+        cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[4.35rem] lg:h-[3.4rem] ${whiteClass}`;
         container.appendChild(cell);
         continue;
       }
 
       const type = cellMap.get(`${r}-${c}`);
 
-      cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[5.375rem] lg:h-[3.8rem] ${
+      cell.className = `w-full h-full md:w-[4rem] md:h-[3.12rem] lg:w-[4.35rem] lg:h-[3.4rem] ${
         type === "yellow" ? yellowClass : ""
       }`;
 
